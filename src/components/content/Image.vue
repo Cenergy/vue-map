15ef4809b0680ce6b01897a2a2ee8c22a946999a
cenergy <673598118@qq.com>
2020-01-11 18:49:46 +0800

    addd

---
+++ src/components/content/Image.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="own-popup">123 ---{{ hello }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hello: 1
+    };
+  }
+};
+</script>
+
+<style>
+.own-popup {
+  padding: 10px;
+  max-width: 200px;
+  background: #fff;
+  border-radius: 4px;
+}
+</style>
